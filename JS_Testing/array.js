11bdc4fa71d562dcb0c77bf6b402eb303d7bea63
Jose Garcia <jagarcia7655@gmail.com>
2020-06-14 00:00:53 -0500

    comentarios en las pruebas

--- JS_Testing/array.js
+++ JS_Testing/array.js
@@ -1,3 +1,20 @@
 var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
 
 console.log(frutas);
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
+
+for (const iterator of object) {
+}
+
+while (condition) {}
+//coemtnario
+function nuevoAuto(marca, modelo, año) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.año = año;
+}
+
+var micarro = new nuevoAuto(renolt, logan, 2018);
