227f3a52f4a89e02a2fd512a159ec31d5912b7db
Jose Garcia <jagarcia7655@gmail.com>
2020-06-21 23:23:05 -0500

    Aprendiendo promesas y mas de los request

--- JS_Testing/callbacks.js
+++ JS_Testing/callbacks.js
@@ -1,11 +1,25 @@
 const API_URL = "https://swapi.dev/api/";
 const PEOPLE_URL = "people/:id";
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const opts = { crossDomain: true };
 
-const onPeopleResponse = function (persona) {
-  console.log(`Hola yo soy, ${persona.name}`);
-};
+function obtenerPersonaje(id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
 
-$.get(lukeUrl, opts, onPeopleResponse);
+    $.get(url, opts, function (data) {
+      resolve(data);
+    }).fail(() => reject(id));
+  });
+}
+
+function onError(id) {
+  console.log(`Hubo un error al obtener el personaje ${id}`);
+}
+
+obtenerPersonaje(1)
+  .then(function (personaje) {
+    console.log(`el personaje 1 es ${personaje.name}`);
+  })
+
+  .catch(onError);
